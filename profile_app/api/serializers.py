f9ab458f870c76af0f0506b9051a3b29e17f98c7
adminhype <ado.designs@outlook.de>
2025-12-04 02:03:52 +0100

    feat: patch profile info

--- profile_app/api/serializers.py
+++ profile_app/api/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
+
+from django.contrib.auth.models import User
+
 from profile_app.models import UserProfile
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(
         source='user.username', read_only=True)
-    email = serializers.EmailField(source='user.email', read_only=True)
+    email = serializers.EmailField(source='user.email')
     type = serializers.CharField(source='user_type', read_only=True)
     user = serializers.IntegerField(source='user.id', read_only=True)
 
@@ -26,3 +29,19 @@ class UserProfileSerializer(serializers.ModelSerializer):
             'created_at'
         ]
         read_only_fields = ['created_at']
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', {})
+        email = user_data.get('email')
+
+        user = instance.user
+
+        if email:
+            if User.objects.filter(email=email).exclude(pk=user.pk).exists():
+                raise serializers.ValidationError(
+                    {"email": "Email already exist."})
+
+        user.email = email
+        user.save()
+
+        return super().update(instance, validated_data)
--- profile_app/api/views.py
+++ profile_app/api/views.py
@@ -5,12 +5,16 @@ from django.shortcuts import get_object_or_404
 
 from profile_app.models import UserProfile
 from profile_app.api.serializers import UserProfileSerializer
+from profile_app.api.permissions import isOwnerOrReadOnly
 
 
-class UserProfileDetailView(generics.RetrieveAPIView):
+class UserProfileDetailView(generics.RetrieveUpdateAPIView):
     serializer_class = UserProfileSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, isOwnerOrReadOnly]
 
     def get_object(self):
         user_id = self.kwargs.get('pk')
-        return get_object_or_404(UserProfile, user__pk=user_id)
+        obj = get_object_or_404(UserProfile, user__pk=user_id)
+
+        self.check_object_permissions(self.request, obj)
+        return obj
